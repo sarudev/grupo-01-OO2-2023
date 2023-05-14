966d295401896d05b8c9395333394dc2abdaebc1
José María Coria <ExB8Ch@gmail.com>
2023-05-14 15:08:12 -0300

    proyecto inicializado

---
+++ backend/src/main/java/com/oo2/grupo01/Grupo01Application.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo01Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Grupo01Application.class, args);
+  }
+
+}
---
+++ backend/src/test/java/com/oo2/grupo01/Grupo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
