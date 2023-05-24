eb4ec29fad600ce9087e44f858d9e39a05f999f6
AdrianEs <escurragustavoadrian@gmail.com>
2023-05-24 18:30:59 -0300

    Funcionalidad a SensorService
    Se agregan las funciones a implementar en ISensorServices
    
    Se crea la funcion convertirSensor(Sensor sensor) que, en base al tipo
    de sensor, va a instanciar el correspondiente para ser retornado y
    mostrar su información en el front
    
    Se agregan las funcionalidades de agregar y eliminar sensor. Asi como
    switchSensor(Long id) que vaa invertir el estado del sensor seleccionado

--- backend/src/main/java/com/oo2/grupo01/services/ISensorService.java
+++ backend/src/main/java/com/oo2/grupo01/services/ISensorService.java
@@ -1,5 +1,15 @@
 package com.oo2.grupo01.services;
 
-public interface ISensorService {
+import com.oo2.grupo01.entities.Lugar;
+import com.oo2.grupo01.entities.Sensor;
+import com.oo2.grupo01.entities.Sensores;
 
+public interface ISensorService {
+	public Sensor traerSensor(Long id);
+	
+	public void switchSensor(Long id);
+	
+	public void agregarSensor(Sensores tipo, Lugar lugar);
+	
+	public void eliminarSensor(Long id);
 }
