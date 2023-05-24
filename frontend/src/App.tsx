49bc228195ff68afd2e88914e89291ccc76c7c24
José María Coria <topchami@gmail.com>
2023-05-24 04:22:15 -0300

    simple routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,36 @@ import './App.scss'
 import { ReactComponent as Campus } from './assets/campus.svg'
 import useBuildingSelector from './hooks/useBuildingSelector'
 import useBuildingNameSelector from './hooks/useBuildingNameSelector'
+import { useLoaderData, createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const campus = {
+  edificios: [
+    {
+      id: 1,
+      nombre: 'José Hernández',
+      sensores: [
+        {
+          id: 1,
+          type: 'tiempo'
+        }
+      ],
+      aulas: [
+        {
+          id: 1,
+          nombre: '11',
+          sensores: [
+            {
+              id: 2,
+              type: 'tiempo'
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  espaciosVerdes: [],
+  parkings: []
+}
 
 function App () {
   const [currentBuilding, setCurrentBuilding] = useState('')
@@ -25,4 +55,53 @@ function MouseCartelito ({ text }: { text: string }) {
   )
 }
 
-export default App
+function Building () {
+  const building = useLoaderData() as typeof campus.edificios
+
+  return (
+    <div>
+      {JSON.stringify(building, null, 2)}
+    </div>
+  )
+}
+
+function Aula () {
+  const aula = useLoaderData() as { id: number, nombre: string, sensores: Array<{ id: number, type: string }> }
+
+  return (
+    <div>
+      {JSON.stringify(aula, null, 2)}
+    </div>
+  )
+}
+
+function Router () {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/edificio/:buildingId',
+    loader: async ({ params }) => {
+      const { buildingId } = params
+      return campus.edificios.find(e => e.id === Number(buildingId))
+    },
+    element: <Building />
+  },
+  {
+    path: '/edificio/:buildingId/aula/:aulaId',
+    loader: async ({ params }) => {
+      const { buildingId, aulaId } = params
+      return campus.edificios.find(e => e.id === Number(buildingId))?.aulas.find(a => a.id === Number(aulaId))
+    },
+    element: <Aula />
+  }
+])
+
+export default Router
