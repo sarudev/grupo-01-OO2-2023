5edc40352caecb1a85fc1e1ddf907ab644fdef35
José María Coria <topchami@gmail.com>
2023-05-25 21:18:01 -0300

    better notfound (styles missing)

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,23 @@
+import { Link, useNavigate } from 'react-router-dom'
+
 export default function NotFound () {
+  const navigate = useNavigate()
+
   return (
-    <div>NotFound</div>
+    <div className="not-found">
+      <div className="code">404</div>
+      <div className="message">Page not found</div>
+      <div className='links'>
+        <Link className='goto' to={'/'}>
+          Return to homepage
+        </Link>
+        <Link className='back' to={'/'} onClick={(e) => {
+          e.preventDefault()
+          navigate(-1)
+        }}>
+          Go back
+        </Link>
+      </div>
+    </div>
   )
 }
