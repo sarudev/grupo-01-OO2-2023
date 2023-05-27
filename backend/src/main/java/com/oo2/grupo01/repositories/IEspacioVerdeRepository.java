2d7bddd2593002e8e1cffab90b21b4c239a2a2b3
Rodrigo <rodri.bri2020@gmail.com>
2023-05-27 02:04:09 -0300

    Se agregaron algunas consultas en repository

--- backend/src/main/java/com/oo2/grupo01/repositories/IEspacioVerdeRepository.java
+++ backend/src/main/java/com/oo2/grupo01/repositories/IEspacioVerdeRepository.java
@@ -3,6 +3,7 @@ package com.oo2.grupo01.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.oo2.grupo01.entities.EspacioVerde;
@@ -11,5 +12,11 @@ import com.oo2.grupo01.entities.EspacioVerde;
 public interface IEspacioVerdeRepository extends JpaRepository<EspacioVerde, Long> {
 	
 	//implementar query que traiga los espacios verdes con sus sensores
+	@Query(value = "SELECT * FROM EspacioVerde ev WHERE ev.ubicacion=?1", nativeQuery = true)
 	public Optional<EspacioVerde> traerPorUbicacion(String ubicacion);
+	
+	
+	public Optional<EspacioVerde> findByUbicacion(String ubicacion);
+	
+	
 }
--- backend/src/main/java/com/oo2/grupo01/repositories/IParkingRepository.java
+++ backend/src/main/java/com/oo2/grupo01/repositories/IParkingRepository.java
@@ -3,6 +3,7 @@ package com.oo2.grupo01.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.oo2.grupo01.entities.Parking;
@@ -12,6 +13,10 @@ public interface IParkingRepository extends JpaRepository<Parking, Long> {
 	
 	//implementar query que traiga el parking con los estacionamientos
 	// y los sensores del parking y de cada estacionamiento
+	@Query(value = "SELECT * FROM Parking p WHERE p.ubicacion=?1", nativeQuery = true)
 	public Optional<Parking> traerPorUbicacion(String ubicacion);
+	
+	
+	public Optional<Parking> findByUbicacion(String ubicacion);
 	
 }
